0f61425a18c2cca65e2dc52ed7632899a3c521c1
JungGuNam <nowage@gmail.com>
2020-12-31 17:27:19 +0900

    up

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /content/
 rm -rf darknet
-!git clone https://github.com/roboflow-ai/darknet.git
+git clone https://github.com/roboflow-ai/darknet.git
 
 rm /content/darknet/Makefile
 cp /content/penDetect/Makefile /content/darknet/
